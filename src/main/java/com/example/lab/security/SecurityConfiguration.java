a165836c5d13f2589b16329faa0982477d7ddb23
Vadim Chernysh <chernysh.vadim@gmail.com>
2021-12-03 09:27:34 +0200

    Allowed admin to add/edit/delete books

---
+++ src/main/java/com/example/lab/security/SecurityConfiguration.java
@@ -0,0 +1,42 @@
+package com.example.lab.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    BCryptPasswordEncoder bCryptEncoder;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        String adminPassword = bCryptEncoder.encode("admin");
+        String userPassword = bCryptEncoder.encode("user");
+        auth.inMemoryAuthentication()
+                .withUser("user").password(userPassword).roles("USER").and()
+                .withUser("admin").password(adminPassword).roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/", "/books").permitAll()
+                .antMatchers("/books/**/edit").hasRole("ADMIN")
+                .antMatchers("/books/**/delete").hasRole("ADMIN")
+                .antMatchers("/books/add").hasRole("ADMIN")
+                .and()
+                .formLogin();
+
+        http       //other configure params.
+                .csrf().disable();
+
+    }
+
+}
